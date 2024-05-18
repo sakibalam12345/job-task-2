a4e011c6ff746c4a39c244e04fcdb51141bcd6e3
sakib ul alam <sakibalambulbul1@gmail.com>
2024-05-18 21:43:21 +0600

    added some style at banner

--- src/Layout/Pages/Banner/Banner.jsx
+++ src/Layout/Pages/Banner/Banner.jsx
@@ -4,14 +4,20 @@ const Banner = () => {
     return (
 
     <div className='banner-container background-overlay'>
-        <div className='max-w-[1600px] mx-auto background-image mt-4 '>
+        <div className='triangle'>
+        </div>
+        
+        <div className='max-w-[1700px] mx-auto background-image mt-4 '>
             <div className="overlay ">
-           
+            
             </div>
+            
            
             
         </div>
+        
         </div>
+        
     );
 };
 
