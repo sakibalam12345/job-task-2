cc09d2f59cd3f8212ea5a6729da4af32d2cb1234
sakib ul alam <sakibalambulbul1@gmail.com>
2024-05-19 17:03:52 +0600

    added text and red layer above the bg img

--- src/Layout/Pages/Banner/Banner.jsx
+++ src/Layout/Pages/Banner/Banner.jsx
@@ -1,21 +1,68 @@
 import './Banner.css'
+import { ImCheckmark } from "react-icons/im";
 
 const Banner = () => {
     return (
 
-    <div className='banner-container background-overlay'>
-        <div className='triangle'>
-        </div>
+
+<div>
+    <div className='banner-container background-overlay '>
+    
+
+        <div className='triangle '> </div> 
+       
         
-        <div className='max-w-[1700px] mx-auto background-image mt-4 '>
+        <div className='max-w-[1700px] mx-auto background-image mt-4 '> 
+       
+
             <div className="overlay ">
+            
+                
+          
             
             </div>
             
            
-            
+           
         </div>
-        
+
+
+        </div>
+        <div className='relative -top-80 z-50'>
+        <div className='max-w-6xl mx-auto '>
+                    {/* left side div */}
+                    <div className='flex justify-between  items-center '>
+
+                   
+                    <div>
+                    <p className='text-white  font-poppins text-base uppercase'>PASS YOUR HOME OFFICE APPROVED TESTS</p>
+                    <h1 className='text-white font-poppins text-6xl font-bold'>
+                    99% PASS <br /> RATE <br />
+
+                   <p className='text-white font-poppins text-7xl font-bold'>FIRST TIME</p>  </h1>
+                   <div className=''>
+                    <p className='text-white font-poppins text-base flex items-center gap-4'><ImCheckmark className='text-[#DB4746] h-8 w-6 '></ImCheckmark>Unlimited online training until you pass</p>
+                    <p className='text-white font-poppins text-base flex items-center gap-4'><ImCheckmark className='text-[#DB4746] h-8 w-6 '></ImCheckmark>Suitable for British citizenship</p>
+                    <p className='text-white font-poppins text-base flex items-center gap-4'><ImCheckmark className='text-[#DB4746] h-8 w-6 '></ImCheckmark>Professional English Teachers</p>
+                    <p className='text-white font-poppins text-base flex items-center gap-4'><ImCheckmark className='text-[#DB4746] h-8 w-6 '></ImCheckmark>All training materials are included</p>
+                    </div>  
+                    <button className=' py-4  px-20 mt-24 bg-white rounded-full'>
+                      <p className='text-base font-poppins font-extrabold text-[#2F346F]'>Get Information</p>  
+                    </button>
+                    
+                    </div>
+                    {/* right side div */}
+                    <div>
+                   <h2 className='text-white font-bold text-3xl'>image and other stuff</h2>
+                    </div>
+
+                    </div>
+                    {/* end */}
+                 
+                </div>
+           
+        </div>
+
         </div>
         
     );
